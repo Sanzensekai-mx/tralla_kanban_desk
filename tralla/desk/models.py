cceb17f631ef43a4797da0baf21b9eda04910530
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2021-06-12 21:30:07 +0400

    Обновления верстки, добавление моделей, отображение пользовательских досок на desk/home

--- tralla/desk/models.py
+++ tralla/desk/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# class Desk(models.Model):
+    # user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+#     # name = User.username
+#     # name = models.CharField(User.username, max_length=150, default='name')
+#     # boards = models.ManyToManyField('Board')
+
+    # class Meta:
+    #     verbose_name = 'Рабочее место'
+    #     verbose_name_plural = 'Рабочие места'
+
+    # def __str__(self):
+    #     return self.user.username
+
+
+class Board(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    # desk = models.ForeignKey(Desk, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    data = models.JSONField()
+
+    class Meta:
+        verbose_name = 'Доска'
+        verbose_name_plural = 'Доски'
+
+    def __str__(self):
+        return self.name
--- tralla/desk/views.py
+++ tralla/desk/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from .models import Board
 
 # Create your views here.
 
 
 @login_required(login_url='/signin')
 def desk_home(request):
-    return render(request, 'desk/home.html')
+    boards = Board.objects.all()
+    return render(request, 'desk/home.html', {'boards': boards})
 
 
 def desk_logout(request):
