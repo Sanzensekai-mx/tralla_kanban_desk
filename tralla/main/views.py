c2308f3b948fe745bfc6d55214e824e1dc53a0e0
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2021-06-16 20:10:42 +0400

    Update README.md и views.py

--- tralla/main/views.py
+++ tralla/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.shortcuts import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm
+from django import forms
 
 
 # Create your views here.
@@ -44,10 +45,20 @@ def sign_up(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
+            try:
+                user_form.clean_pass2()
+            except forms.ValidationError:
+                user_form = UserRegistrationForm()
+                return render(request, 'main/signup.html', {'user_form': user_form,
+                                                            'not_valid': 'Пароли не совпадают.'})
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             return render(request, 'main/signup_done.html', {'new_user': new_user})
+        else:
+            user_form = UserRegistrationForm()
+            return render(request, 'main/signup.html', {'user_form': user_form,
+                                                        'not_valid': 'Такое имя пользователя уже занято.'})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'main/signup.html', {'user_form': user_form})
